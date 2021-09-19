b909689dcd0b08f2e451e85074e17c287c5ca8bf
peanut <z40140110z@gmail.com>
2021-09-19 16:49:01 +0800

    recursive baby!

--- practice/sum.js
+++ practice/sum.js
@@ -1,6 +1,14 @@
 console.log("hello world!");
 
-function sum(num) {
+function sum(n) {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(n - 1) + n;
+  }
+}
+
+function sum2(num) {
   let result = 0;
   while (num > 0) {
     result += num;
