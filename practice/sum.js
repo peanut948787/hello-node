c602f8d71be54c44ac66324975cfe1960903cc93
peanut <z40140110z@gmail.com>
2021-09-19 17:18:22 +0800

    ternary recursion just for fun

--- practice/sum.js
+++ practice/sum.js
@@ -1,11 +1,7 @@
 console.log("hello recursive!");
 
 function sum(n) {
-  if (n <= 0) {
-    return 0;
-  } else {
-    return sum(n - 1) + n;
-  }
+  return n <= 0 ? 0 : sum(n - 1) + n;
 }
 
 let result = sum(10);
