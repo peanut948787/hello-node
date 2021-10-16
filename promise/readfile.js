e97f673c9b17d3e9f44060a8e1b87aaa96ecf04a
peanut <z40140110z@gmail.com>
2021-10-16 10:48:20 +0800

    formatting readfile.js

--- promise/readfile.js
+++ promise/readfile.js
@@ -8,17 +8,18 @@ const fs = require("fs");
 //   }
 // });
 
-function readFilePromise () {
-    return new Promise ((resolve,reject)=>{
-        fs.readFile("input.txt", "utf-8", (err,data)=>{
-            if(err){
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
+function readFilePromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("input.txt", "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
-readFilePromise().then((data)=>console.log("success! "+data)).catch(err=> console.log("failed! " + err))
-
+readFilePromise()
+  .then((data) => console.log("success! " + data))
+  .catch((err) => console.log("failed! " + err));
